348e9764890efadff9945caa46ab7e571d70da13
ssizan01 <showvik.sizan@gmail.com>
2021-06-25 15:06:35 -0400

    trying to cache initial credentials in a new file.

---
+++ src/initial_credentials.py
@@ -0,0 +1,24 @@
+# Initialize the clients
+import os
+#import streamlit as st
+from google.api_core.client_options import ClientOptions
+from google.cloud import bigquery
+from google.cloud.dataqna import AutoSuggestionServiceClient, Question, QuestionServiceClient, SuggestQueriesRequest, UpdateUserFeedbackRequest, UserFeedback
+from google.cloud import bigquery_storage
+from config import project_name,tables_list
+from pathlib import Path
+from glob import glob
+#import altair as alt
+
+base_path = Path(__file__).parent
+secrets_path = (base_path / "../secrets").resolve()
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = glob(str(secrets_path) + "/*.json")[0]
+#print(glob(str(secrets_path) + "/*.json")[1])
+location = "us"  # "us" or "eu"
+client_options = ClientOptions(api_endpoint=f"{location}-dataqna.googleapis.com:443")
+suggest_client = AutoSuggestionServiceClient(client_options=client_options)
+questions_client = QuestionServiceClient(client_options=client_options)
+bq_client = bigquery.Client()
+bqstorageclient = bigquery_storage.BigQueryReadClient()
+parent = questions_client.common_location_path(project_name, location)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
-import os
+#import os
 import streamlit as st
-from google.api_core.client_options import ClientOptions
-from google.cloud import bigquery
-from google.cloud.dataqna import AutoSuggestionServiceClient, Question, QuestionServiceClient, SuggestQueriesRequest, UpdateUserFeedbackRequest, UserFeedback
-from google.cloud import bigquery_storage
-from config import project_name,tables_list
-from pathlib import Path
-from glob import glob
+# from google.api_core.client_options import ClientOptions
+# from google.cloud import bigquery
+# from google.cloud.dataqna import AutoSuggestionServiceClient, Question, QuestionServiceClient, SuggestQueriesRequest, UpdateUserFeedbackRequest, UserFeedback
+# from google.cloud import bigquery_storage
+# from config import project_name,tables_list
+# from pathlib import Path
+# from glob import glob
 import altair as alt
+from initial_credentials import *
 
-base_path = Path(__file__).parent
-secrets_path = (base_path / "../secrets").resolve()
+# base_path = Path(__file__).parent
+# secrets_path = (base_path / "../secrets").resolve()
 
 
 table_dic ={}
@@ -23,16 +24,16 @@ dataset_name = st.sidebar.selectbox(
     list(table_dic.keys())
 )
 
-# Initialize the clients
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = glob(str(secrets_path) + "/*.json")[0]
-#print(glob(str(secrets_path) + "/*.json")[1])
-location = "us"  # "us" or "eu"
-client_options = ClientOptions(api_endpoint=f"{location}-dataqna.googleapis.com:443")
-suggest_client = AutoSuggestionServiceClient(client_options=client_options)
-questions_client = QuestionServiceClient(client_options=client_options)
-bq_client = bigquery.Client()
-bqstorageclient = bigquery_storage.BigQueryReadClient()
-parent = questions_client.common_location_path(project_name, location)
+# # Initialize the clients
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = glob(str(secrets_path) + "/*.json")[0]
+# #print(glob(str(secrets_path) + "/*.json")[1])
+# location = "us"  # "us" or "eu"
+# client_options = ClientOptions(api_endpoint=f"{location}-dataqna.googleapis.com:443")
+# suggest_client = AutoSuggestionServiceClient(client_options=client_options)
+# questions_client = QuestionServiceClient(client_options=client_options)
+# bq_client = bigquery.Client()
+# bqstorageclient = bigquery_storage.BigQueryReadClient()
+# parent = questions_client.common_location_path(project_name, location)
 
 
 #@st.cache()
@@ -149,6 +150,7 @@ def main():
 #
 # else:
 #     main()
+
 
 
 main()
