12db1c5cd08846587fdf8518c76bcca7bdf3f7b9
salahayoubi <46683470+salahayoubi@users.noreply.github.com>
2021-10-07 11:58:50 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,4 +3,4 @@
 #
 
 project_name = "bigquery-data-qna-webapp-demo"
-tables_list = ['DataQnA.Chicago_Taxi_Trips','DataQnA.GA_Sessions','DataQnA.Iowa_Liquor_Sales','DataQnA.NYC_311_SRs'] ##in the format 'dataset_name.table_name'. For example ['lab.iowa_liquor_sales','lab.chicago_taxi_trips']
+tables_list = ['DataQnA.Iowa_Liquor_Sales','DataQnA.Chicago_Taxi_Trips','DataQnA.NYC_311_SRs','DataQnA.GA_Sessions','DataQnA.GoogleTrends_TopRisingTerms','DataQnA.GoogleTrends_TopTerms','DataQnA.Loans'] ##in the format 'dataset_name.table_name'. For example ['lab.iowa_liquor_sales','lab.chicago_taxi_trips']
